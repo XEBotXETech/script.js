93243d4586243baa4c59ef041e7ef65b566da288
XEBotXETech <136056870+XEBotXETech@users.noreply.github.com>
2023-07-02 11:10:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function downloadReel() {
-    var videoUrl = prompt("Enter the Instagram Reel video URL:");
+function downloadVideo() {
+    var urlInput = document.getElementById("urlInput");
+    var videoUrl = urlInput.value;
+
     if (videoUrl) {
-        var a = document.createElement('a');
-        a.href = videoUrl;
-        a.download = 'instagram_reel.mp4';
-        a.click();
+        var videoId = videoUrl.split("/")[4];
+        var videoApiUrl = "https://www.instagram.com/p/" + videoId + "/?__a=1";
+
+        fetch(videoApiUrl)
+            .then(response => response.json())
+            .then(data => {
+                var videoUrl = data.graphql.shortcode_media.video_url;
+                var videoElement = document.createElement("video");
+                videoElement.controls = true;
+                videoElement.src = videoUrl;
+
+                var videoContainer = document.getElementById("videoContainer");
+                videoContainer.innerHTML = "";
+                videoContainer.appendChild(videoElement);
+            })
+            .catch(error => {
+                console.log("An error occurred: ", error);
+            });
     }
 }
